116cb02a79d3c69c7ff9d361a884ad64f2912f55
Dennis Holzmann <dennis.holzmann@uni-bielefeld.de>
2025-06-12 15:11:30 +0200

    fix pseudo label selection and no-obj label

--- models/fs_detr.py
+++ models/fs_detr.py
@@ -189,7 +189,7 @@ class ConditionalDETR(nn.Module):
             classes.update(list(bt.keys()))
 
         max_num_classes = len(classes)  # total number of classes present in the batch
-        pseudo_classes = np.random.choice(range(self.num_classes), max_num_classes, replace=False)
+        pseudo_classes = np.random.choice(range(self.num_classes-1), max_num_classes, replace=False)
         label2pseudo = {c: int(p) for c, p in zip(classes, pseudo_classes)}
         # append last class as no object class
         return label2pseudo
